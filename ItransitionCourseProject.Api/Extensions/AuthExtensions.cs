8416191017bdad83810819f8294cf20d233e48f6
bekasker <2004bek.asker@mail.ru>
2026-03-12 23:15:31 +0500

    feat: setup identity server and auth via extension classes

---
+++ ItransitionCourseProject.Api/Extensions/AuthExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ItransitionCourseProject.Api.Extensions;
+
+public static class AuthExtensions
+{
+    public static IServiceCollection AddApiAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        if (configuration.GetValue<bool>("FeatureManagement:EnableGoogleOAuth"))
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = configuration["Authentication:Google:ClientId"]!;
+                    options.ClientSecret = configuration["Authentication:Google:ClientSecret"]!;
+                });
+        }
+        else
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = BearerTokenDefaults.AuthenticationScheme;
+                })
+                .AddBearerToken();
+        }
+
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+
+            options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+
+        return services;
+    }
+}
---
+++ ItransitionCourseProject.Api/Extensions/IdentityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ItransitionCourseProject.Api.Data;
+using ItransitionCourseProject.Api.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ItransitionCourseProject.Api.Extensions;
+
+public static class IdentityExtensions
+{
+    public static IServiceCollection AddApiIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+
+                if (!configuration.GetValue<bool>("FeatureManagement:EnableGoogleOAuth"))
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 1;
+                }
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
