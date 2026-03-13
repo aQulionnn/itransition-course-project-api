0a342e968ef3241fdedba7359de0fa9610203c1c
bekasker <2004bek.asker@mail.ru>
2026-03-13 05:53:52 +0500

    test: add domain models design architecture test

---
+++ ItransitionCourseProject.Api/AssemblyReference.cs
@@ -0,0 +1,5 @@
+﻿namespace ItransitionCourseProject.Api;
+
+public class AssemblyReference
+{
+}
---
+++ ItransitionCourseProject.ArchitectureTests/DomainModelsDesignTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using NetArchTest.Rules;
+
+namespace ItransitionCourseProject.ArchitectureTests;
+
+public class DomainModelsDesignTests
+{
+    private const string DomainModelsNamespace = "ItransitionCourseProject.Api.Models";
+    private readonly Assembly _assembly = typeof(Api.AssemblyReference).Assembly;
+    
+    [Fact]
+    public void Domain_Models_Should_Be_Sealed_Classes()
+    {
+        var result = Types
+            .InAssembly(_assembly)
+            .That()
+            .AreClasses()
+            .And()
+            .ResideInNamespace(DomainModelsNamespace)
+            .Should()
+            .BeSealed()
+            .GetResult();
+        
+        var failingTypes = (result.FailingTypes ?? Enumerable.Empty<Type>())
+            .Select(t => t.FullName);
+        
+        Assert.True(result.IsSuccessful, string.Join(Environment.NewLine, failingTypes));
+    }
+}
