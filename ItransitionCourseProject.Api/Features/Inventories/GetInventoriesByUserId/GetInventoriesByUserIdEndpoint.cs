7c41e846a320346994b1e6e721f8644a5b248d0c
bekasker <2004bek.asker@mail.ru>
2026-03-13 07:39:19 +0500

    refactor: update GetInventoriesByUserIdEndpoint

--- ItransitionCourseProject.Api/Features/Inventories/GetInventoriesByUserId/GetInventoriesByUserIdEndpoint.cs
+++ ItransitionCourseProject.Api/Features/Inventories/GetInventoriesByUserId/GetInventoriesByUserIdEndpoint.cs
@@ -1,7 +1,5 @@
-﻿// Features/Inventories/GetInventoriesByUserId/GetInventoriesByUserIdEndpoint.cs
-using Carter;
+﻿using Carter;
 using ItransitionCourseProject.Api.Data;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
@@ -19,19 +17,22 @@ public class GetInventoriesByUserIdEndpoint : ICarterModule
             var currentUserId = ctx.User.FindFirstValue(ClaimTypes.NameIdentifier);
             var isAdmin = ctx.User.IsInRole("Admin");
 
+            var user = await db.Users
+                .AsNoTracking()
+                .Where(u => u.Id == userId)
+                .Select(u => new { u.UserName })
+                .FirstOrDefaultAsync();
+
+            if (user is null) return Results.NotFound();
+
             var inventories = await db.Inventories
-                .Include(i => i.Category)
-                .Include(i => i.Items)
-                .Include(i => i.InventoryTags)
-                .ThenInclude(t => t.Tag)
+                .AsNoTracking()
                 .Where(i =>
                     i.CreatorId == userId &&
-                    (
-                        isAdmin ||
-                        i.IsPublic ||
-                        i.CreatorId == currentUserId ||
-                        i.InventoryAccesses.Any(a => a.UserId == currentUserId)
-                    ))
+                    (isAdmin ||
+                     i.IsPublic ||
+                     i.CreatorId == currentUserId ||
+                     i.InventoryAccesses.Any(a => a.UserId == currentUserId)))
                 .Select(i => new GetInventoriesByUserIdResponse(
                     i.Id,
                     i.Title,
@@ -44,7 +45,7 @@ public class GetInventoriesByUserIdEndpoint : ICarterModule
                 ))
                 .ToListAsync();
 
-            return Results.Ok(inventories);
+            return Results.Ok(new { userName = user.UserName, inventories });
         }).AllowAnonymous().WithTags("Inventories");
     }
 }
