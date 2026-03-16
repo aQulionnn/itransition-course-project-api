2530d0bc0bc8fc0c3e9d28e19bdf915b2cee22fd
bekasker <2004bek.asker@mail.ru>
2026-03-16 16:59:57 +0500

    feat: add POST /api/images/upload endpoint

---
+++ ItransitionCourseProject.Api/Features/Images/UploadImage/UploadImageEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using Carter;
+using ItransitionCourseProject.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItransitionCourseProject.Api.Features.Images.UploadImage;
+
+public class UploadImageEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/images/upload", async ([FromForm] IFormFile file, ClaimsPrincipal principal,
+                UserManager<AppUser> userManager, BlobServiceClient blobServiceClient) =>
+            {
+                var userId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+                if (userId is null)
+                    return Results.Unauthorized();
+
+                var user = await userManager.GetUserAsync(principal);
+                if (user is null)
+                    return Results.Unauthorized();
+
+                var container = blobServiceClient.GetBlobContainerClient("images");
+
+                var blobName = $"users/{userId}/{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+                await container.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+                var blob = container.GetBlobClient(blobName);
+
+                await blob.UploadAsync(file.OpenReadStream(), new BlobUploadOptions
+                {
+                    HttpHeaders = new BlobHttpHeaders
+                    {
+                        ContentType = file.ContentType
+                    }
+                });
+
+                return Results.Ok(new { url = blob.Uri });
+            })
+            .DisableAntiforgery()
+            .WithTags("Images");
+    }
+}
--- ItransitionCourseProject.Api/Program.cs
+++ ItransitionCourseProject.Api/Program.cs
@@ -3,6 +3,8 @@ using Carter;
 using DotNetEnv;
 using ItransitionCourseProject.Api.Extensions;
 using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Azure;
 using Microsoft.FeatureManagement;
 using Scalar.AspNetCore;
 
@@ -20,6 +22,21 @@ if (!builder.Environment.IsDevelopment())
     var keyVaultUri = new Uri(builder.Configuration["KeyVault:Uri"]!);
     builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
 }
+
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration.GetConnectionString("BlobStorage"));
+});
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.MaxDepth = 128;
+});
+
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 10 * 1024 * 1024;
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -70,8 +87,7 @@ app.MapScalarApiReference("/scalar", options =>
         .AddAuthorizationCodeFlow(BearerTokenDefaults.AuthenticationScheme,
             flow =>
             {
-                flow.Token =
-                    "CfDJ8DNoxjbbLI5MpNKLIgUgqrXC_N0xX1w-sxp15GBBhNGZU63WSgzh1xu9Muv2PmfTVv9BMZjY6ksIlkZX68Ezm02AeoGCBflxNfOgr5RVA7cnje9WhXRdMiUM45dRBBpMRA7VCrvqN-2yvHl5lPrb-ZyixpD_q0QlWFLkTuchHW-Fy0HN4A0uLrW5yuj12oZxoT5o_h-oYAE9Aw9mEKQE1pz6OdPErOQePSB4X1EP42hCViHGZJy1KGx-Xcizt82NNmJocUL1G45yOXTXR1bvekvRDbCrthJpm77o7DcXI1OlvH0oLg9u6Jc-6O10GCD7CC0wT_CvjtgdnKGbDZHSE_NjAjjCtr4PdCfsnuqB33UYkBVuQqCNU14X67StgjMF-HLTHKsmhZO3s2Rx-zWqJO4kj_ONG-mCbch-dlHIuzy__dQ69W_xaas_l2bQSSWOwms7TfFFqdfiC1sg7Sqxr5ayvwLMMHVjn_nOf39yTeaii4aBa0_UrwudLsk54XKBomuGR2xFCcEERAVud0qqtDjKBPyLSh_qTNTBx9wan5Nj";
+                flow.Token = "";
             });
 }).AllowAnonymous();
 
